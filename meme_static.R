c5535a59f166f8314d304b14dcd0d8655c076f1d
Drewporter03 <99791836+Drewporter03@users.noreply.github.com>
2024-03-04 19:18:58 +1300

    add comments to meme_static.R

--- meme_static.R
+++ meme_static.R
@@ -1,5 +1,6 @@
 library(magick)
 
+#add images from device and scale them 
 good_hair <- image_read("C:/Users/porte/OneDrive - The University of Auckland/STATS 220/good_hair.jpg")%>%
   image_scale(200)
 
@@ -15,6 +16,8 @@ mid_cut <- image_read("C:/Users/porte/OneDrive - The University of Auckland/STAT
 after_cut <- image_read("C:/Users/porte/OneDrive - The University of Auckland/STATS 220/after_cut.jpg") %>%
   image_scale(200)
 
+#add blank images the same size as other images
+#add text describing the situation
 good_hair_txt <- image_blank(200, 
                    200, 
                    color = "white")%>%
@@ -60,24 +63,30 @@ after_cut_txt<- image_blank(200,
                  font="impact",
                  gravity = "center")
 
-
+#create a vector for the photos
 hair_vector <- c(good_hair,
                  needs_cut,
                  in_barber,
                  mid_cut,
                  after_cut)
+
+#create a vector for the text images
 txt_vector <- c(good_hair_txt,
                 needs_cut_txt,
                 in_barber_txt,
                 mid_cut_txt,
                 after_cut_txt)
 
+#stack the images
 stacked_images <- image_append(hair_vector, stack = TRUE)
 
+#stack the text images
 stacked_txt<- image_append(txt_vector, stack=TRUE)
 
+#create a vector of the two stacks
 combined_txt_images <- c(stacked_txt, stacked_images)
 
+#place the two stacks side by side
 meme <-image_append(combined_txt_images)
 
 image_write(meme, "my_meme.png")
